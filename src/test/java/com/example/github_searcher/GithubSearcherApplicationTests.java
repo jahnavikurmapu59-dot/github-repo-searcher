242e5ec381f88a75735dbd1f280a19d12fc23091
jahnavikurmapu59-dot <jahnavikurmapu59@gmail.com>
2026-04-14 19:33:29 +0530

    Update GithubSearcherApplicationTests.java

--- src/test/java/com/example/github_searcher/GithubSearcherApplicationTests.java
+++ src/test/java/com/example/github_searcher/GithubSearcherApplicationTests.java
@@ -2,11 +2,7 @@ package com.example.github_searcher;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-int stars = repo.getStargazersCount();
 
-String category = (stars > 500) ? "🔥 Trending" : "📉 Low Popularity";
-
-repositoryEntity.setCategory(category);
 @SpringBootTest
 class GithubSearcherApplicationTests {
 
